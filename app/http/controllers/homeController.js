ff4e689e33d7ed62b7cff1dbbfc04e4459c0d78b
SparshShukla <88098242+SparshShukla@users.noreply.github.com>
2023-02-27 23:39:51 +0530

    updated db url

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,12 +3,7 @@ const Menu = require('../../models/menu');
 function homeController(){
     return {
         async index(req,res) {
-            // Menu.find().then(function(pizzas){
-            //     console.log(pizzas)
-            //     return res.render('home',{pizzas: pizzas})
-            // })
             const pizzas = await Menu.find()
-            // console.log(pizzas)
             return res.render('home',{pizzas: pizzas})
         }
     }
